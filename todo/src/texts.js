b6e3b1a57de11b5327546b4feaecfc763ea0e2c3
DzejDzej33 <jarek.jazienicki@gmail.com>
2018-05-14 21:26:37 +0200

    .

--- todo/src/texts.js
+++
@@ -1,5 +0,0 @@
-export const TEXTS = Object.freeze({
-    HELLO: "Hello",
-    MY_NAME_IS: "My name is"
-  });
-  
--- todo/src/utils.js
+++
@@ -1,9 +0,0 @@
-import { TEXTS } from './texts';
-
-export function printToConsole(text) {
-  console.log(text);
-}
-
-export function sayHello(name) {
-  printToConsole(TEXTS.HELLO) + printToConsole(name)
-}
